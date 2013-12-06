8e5cb762b279da5ad379b497b83a31c37b62c117
kg <kennytgao@gmail.com>
2013-12-06 15:17:56 -0800

    rails new & rails generate controller welcome index

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blokg::Application.config.secret_key_base = '31974f2c06da5475c375a00ab0840abc4f8f2ef6f8c8ffc6d4e0c9c52be85f667e06345be1b2647f01f60501b852288442ffd9c78fd2f9f8b0040209fff2ece9'
